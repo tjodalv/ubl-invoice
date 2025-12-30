ba8ae878de13402b5bab59aa9322698bb7a68930
Zvonimir Lokmer <lokmer@gmail.com>
2025-12-30 19:54:29 +0100

    feat: Added ext namespace and support for custom ns for Credit Note docs

--- src/Generator.php
+++ src/Generator.php
@@ -29,17 +29,21 @@ class Generator
         ]);
     }
 
-    public static function creditNote(CreditNote $creditNote, $currencyId = 'EUR')
+    public static function creditNote(CreditNote $creditNote, $currencyId = 'EUR', array $customNamespaces = [])
     {
         self::$currencyID = $currencyId;
 
         $xmlService = new Service();
 
-        $xmlService->namespaceMap = [
-            'urn:oasis:names:specification:ubl:schema:xsd:' . $creditNote->xmlTagName . '-2' => '',
-            'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'           => 'cbc',
-            'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'       => 'cac'
-        ];
+        $xmlService->namespaceMap = array_merge(
+            [
+                'urn:oasis:names:specification:ubl:schema:xsd:' . $creditNote->xmlTagName . '-2' => '',
+                'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'           => 'cbc',
+                'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'       => 'cac',
+                'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2'       => 'ext',
+            ],
+            $customNamespaces
+        );
 
         return $xmlService->write($creditNote->xmlTagName, [
             $creditNote
